253fbbc7184676d4de8a1a7037a38c5fb657410e
njxue <ngjxue@gmail.com>
2022-08-18 19:36:53 +0800

    add skeleton register

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,29 +1,61 @@
 import {
-    FormControl,
-    FormLabel,
-    Heading,
-    Input,
-    VStack,
-  } from "@chakra-ui/react";
-  
-  interface Props {}
-  export const Register: React.FC<Props> = () => {
-    return (
-      <VStack>
-        <Heading>Register</Heading>
-        <FormControl>
-          <FormLabel>Email: </FormLabel>
-          <Input type="text" />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Password: </FormLabel>
-          <Input type="password" />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Confirm Password: </FormLabel>
-          <Input type="password" />
-        </FormControl>
-      </VStack>
-    );
-  };
-  
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import { useRef } from "react";
+import useAuth from "../../contexts/AuthContext";
+
+interface Props {}
+export const Register: React.FC<Props> = () => {
+  let auth = useAuth();
+  let currUser: User;
+  let register: (
+    email: string,
+    password: string,
+    username: string
+  ) => Promise<any>;
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const passwordCfRef = useRef<HTMLInputElement>(null);
+
+  if (auth) {
+    if (auth.currUser) {
+      currUser = auth.currUser;
+    }
+    register = auth.register;
+    
+  }
+
+  function handleRegister() {
+    // TODO: enforce all input fields are filled
+    const email: string = emailRef.current!.value;
+    const password: string = passwordRef.current!.value;
+    const passwordcf: string = passwordCfRef.current!.value;
+    register(email, password, passwordcf);
+  }
+
+  return (
+    <VStack>
+      <Heading>Register</Heading>
+      <FormControl>
+        <FormLabel>Email: </FormLabel>
+        <Input ref={emailRef} type="text" />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Password: </FormLabel>
+        <Input ref={passwordRef} type="password" />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Confirm Password: </FormLabel>
+        <Input ref={passwordCfRef} type="password" />
+      </FormControl>
+      <Button onClick={handleRegister}>Register</Button>
+    </VStack>
+  );
+};
--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,18 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
+import "firebase/compat/database";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBXFUnYP6MDr_E_G59HnG4nhxkBv6uSgSE",
   authDomain: "todo-7632c.firebaseapp.com",
   projectId: "todo-7632c",
   storageBucket: "todo-7632c.appspot.com",
   messagingSenderId: "466976857763",
-  appId: "1:466976857763:web:f207e732fa9ce9852ca916"
+  appId: "1:466976857763:web:f207e732fa9ce9852ca916",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export {}
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const ref = firebase.database().ref();
+
+export default app;
