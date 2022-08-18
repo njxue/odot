62cd79d08548788fa210862ca5ce530e046e111b
njxue <ngjxue@gmail.com>
2022-08-18 19:37:28 +0800

    Add AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useState, useEffect, ContextType } from "react";
+import {
+  sendEmailVerification,
+  updateProfile,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  User,
+  Unsubscribe,
+} from "firebase/auth";
+import { auth, ref } from "../config/firebase";
+
+interface AuthContextType {
+  currUser: User | undefined;
+  register: (email: string, password: string, username: string) => Promise<any>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export default function useAuth(): AuthContextType | undefined {
+  return useContext(AuthContext);
+}
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<Props> = ({ children }) => {
+  const [currUser, setCurrUser] = useState<User | undefined>();
+  useEffect(() => {
+    const unsubscribe: Unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrUser(user);
+      } else {
+        setCurrUser(undefined);
+      }
+    });
+    return unsubscribe;
+  }, [])
+  const value: AuthContextType = {currUser, register};
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+function register(
+  email: string,
+  password: string,
+  username: string
+): Promise<any> {
+  return createUserWithEmailAndPassword(auth, email, password).then((cred) => {
+    updateProfile(cred.user, {
+      displayName: username,
+    });
+  });
+}
+
+ 
