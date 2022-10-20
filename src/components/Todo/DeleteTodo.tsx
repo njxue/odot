3e43ef5554f5bf28a50391c60fa11eda2e7e0e10
njxue <ngjxue@gmail.com>
2022-10-20 22:39:39 +0800

    Display confirmation modal before deleting todo

--- src/components/Todo/DeleteTodo.tsx
+++ src/components/Todo/DeleteTodo.tsx
@@ -1,20 +1,31 @@
+import { useDisclosure } from "@chakra-ui/react";
 import { remove } from "firebase/database";
 import useAuth from "../../contexts/AuthContext";
 import { getTodoRef } from "../../helpers/refs";
 import styles from "../../styles/Button.module.css";
+import ConfirmationModal from "../layout/ConfirmationModal";
 
 const DeleteTodo: React.FC<{ todoId: string }> = (props) => {
   const { todoId } = props;
   const todoRef = getTodoRef(useAuth().getCurrUser().uid, todoId);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   function handleDelete(): void {
     remove(todoRef);
   }
 
   return (
-    <button className={styles.removeBtn} onClick={handleDelete}>
-      Delete this todo
-    </button>
+    <>
+      <button className={`${styles.btn} ${styles.removeBtn}`} onClick={onOpen}>
+        Delete this todo
+      </button>
+      <ConfirmationModal
+        actionToConfirm="delete this todo"
+        isOpen={isOpen}
+        onClose={onClose}
+        actionOnConfirm={handleDelete}
+      />
+    </>
   );
 };
 
---
+++ src/components/layout/ConfirmationModal.tsx
@@ -0,0 +1,56 @@
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  ButtonGroup,
+  Button,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+
+interface ModalProps {
+  actionToConfirm: string;
+  isOpen: boolean;
+  actionOnConfirm: () => any;
+  onClose: () => any;
+}
+const ConfirmationModal: React.FC<ModalProps> = (props) => {
+  const { isOpen, actionToConfirm, onClose, actionOnConfirm } = props;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Confirm</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>
+            Are you sure you want to <b>{actionToConfirm}</b> ?
+          </Text>
+          <ButtonGroup w="100%" display="flex" marginTop={5}>
+            <Button
+              onClick={actionOnConfirm}
+              colorScheme="green"
+              flexGrow={1}
+              flexBasis={0}
+            >
+              Confirm
+            </Button>
+            <Button
+              onClick={onClose}
+              colorScheme="gray"
+              flexGrow={1}
+              flexBasis={0}
+            >
+              Cancel
+            </Button>
+          </ButtonGroup>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
