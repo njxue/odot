c38909f9a101b5f2f6be3fdcc93e5e035bd9a327
njxue <ngjxue@gmail.com>
2022-12-21 16:36:14 +0800

    Change styling

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -22,6 +22,7 @@ import {
 } from "@chakra-ui/react";
 import TaskList from "../TaskList";
 import { TasksBoard } from "./TasksBoard";
+import { useWindowDimensions } from "../../helpers/windowDimensions";
 
 export const Dashboard: React.FC<{}> = () => {
   const currUser: User = useAuth().getCurrUser();
@@ -35,6 +36,8 @@ export const Dashboard: React.FC<{}> = () => {
   const [completedTasks, setCompletedTasks] = useState<ITask[] | undefined>(
     undefined
   );
+
+  const w = useWindowDimensions().width;
 
   useEffect(() => {
     onValue(todosRef, (snapshot) => {
@@ -80,16 +83,22 @@ export const Dashboard: React.FC<{}> = () => {
     completedTasks == undefined ? (
     <Loader />
   ) : (
-    <Tabs orientation="vertical" w="100%" display="flex" h="100%">
-      <TabList w="100%" flexGrow={1} flexBasis={0} paddingTop={5}>
+    <Tabs
+      orientation={w < 500 ? "horizontal" : "vertical"}
+      w="100%"
+      display="flex"
+      flexDir={w < 500 ? "column" : "row"}
+      h="100%"
+    >
+      <TabList w="100%" paddingTop={5} flexGrow={1} flexBasis={0} minW="200px">
         <Tab w="100%">Organised</Tab>
         <Tab w="100%">All</Tab>
         <Tab w="100%">Important</Tab>
         <Tab w="100%">Today</Tab>
         <Tab w="100%">Completed</Tab>
       </TabList>
-      <Divider orientation="vertical" borderColor="black" />
-      <TabPanels flexGrow={5} flexBasis={0} h="100%" overflow="hidden">
+      {w >= 500 && <Divider orientation="vertical" borderColor="black" />}
+      <TabPanels flexGrow={1} h="100%" overflow="hidden">
         <TabPanel h="100%">
           <Flex direction="column" maxH="100%">
             <Heading>Welcome back!</Heading>
