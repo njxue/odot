9eb497119ca66b73af725fc4f499d954ae1d24a7
njxue <ngjxue@gmail.com>
2022-12-25 16:25:59 +0800

    Change loader

--- src/components/layout/Loader.tsx
+++ src/components/layout/Loader.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Image } from "@chakra-ui/react";
+import { HStack, Center, Image, Box } from "@chakra-ui/react";
 
 const Loader: React.FC<{}> = () => {
   return (
@@ -13,13 +13,48 @@ const Loader: React.FC<{}> = () => {
       bg="#00494c"
       zIndex={2147483647}
     >
-      <Box w="30%" h="30%">
+      <HStack
+        w="100%"
+        display="flex"
+        h="20%"
+        flexWrap="wrap-reverse"
+        alignContent="center"
+        alignItems="center"
+        justifyContent="center"
+      >
         <Image
           src={require("../../assets/logo.png")}
-          boxSize="100%"
+          boxSize="200px"
+          padding={0}
           objectFit="contain"
         />
-      </Box>
+        <Center>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ width: "100px", height: "100px" }}
+          >
+            <circle
+              cx="50"
+              cy="50"
+              r="20"
+              stroke-width="6"
+              stroke="#ffffff"
+              stroke-dasharray="54.97787143782138 54.97787143782138"
+              fill="none"
+              stroke-linecap="round"
+            >
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                repeatCount="indefinite"
+                dur="0.7042253521126761s"
+                keyTimes="0;1"
+                values="0 50 50;360 50 50"
+              ></animateTransform>
+            </circle>
+          </svg>
+        </Center>
+      </HStack>
     </Center>
   );
 };
