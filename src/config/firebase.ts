d0e91587153f3fceed69a9ff9e2c47b8372b8f7b
njxue <ngjxue@gmail.com>
2022-12-30 16:54:08 +0800

    Use environment variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase} from "firebase/database";
+import { getDatabase } from "firebase/database";
 import "firebase/auth";
 import "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXFUnYP6MDr_E_G59HnG4nhxkBv6uSgSE",
-  authDomain: "todo-7632c.firebaseapp.com",
-  projectId: "todo-7632c",
-  storageBucket: "todo-7632c.appspot.com",
-  messagingSenderId: "466976857763",
-  appId: "1:466976857763:web:f207e732fa9ce9852ca916",
-  databaseURL: "https://todo-7632c-default-rtdb.firebaseio.com"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
 // Initialize Firebase
